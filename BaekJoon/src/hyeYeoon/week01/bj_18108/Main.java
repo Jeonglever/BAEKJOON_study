d4e3d5d5b8ed0bb0f63ed0ef2ee81154701281c9
정혜연 <jeonglever@gmail.com>
2023-10-24 19:25:23 +0900

    Add problem 18108

---
+++ BaekJoon/src/hyeYeoon/week01/bj_18108/Main.java
@@ -0,0 +1,18 @@
+package hyeYeoon.week01.bj_18108;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		
+		System.out.println(A-543);
+		
+		
+	}
+
+}
