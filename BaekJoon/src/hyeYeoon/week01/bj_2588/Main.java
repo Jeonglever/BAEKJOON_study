3d4653ebbd4a5d9edffd75a23460324eca9b0191
정혜연 <jeonglever@gmail.com>
2023-10-25 17:18:16 +0900

    Update problem 2588

--- BaekJoon/src/hyeYeoon/week01/bj_2588/Main.java
+++ BaekJoon/src/hyeYeoon/week01/bj_2588/Main.java
@@ -1,20 +1,43 @@
 package hyeYeoon.week01.bj_2588;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//import java.util.Scanner;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		Scanner sc = new Scanner(System.in);
+		int A = Integer.parseInt(bf.readLine());
+		String B = bf.readLine();
 		
-		int A = sc.nextInt();
-		String B = sc.next();
 		
-		System.out.println(A * (B.charAt(2) - '0'));
-		System.out.println(A * (B.charAt(1) - '0'));
-		System.out.println(A * (B.charAt(0) - '0'));
-		System.out.println(A * Integer.parseInt(B));
+		bw.write(String.valueOf(A * (B.charAt(2) - '0'))+"\n");
+		bw.write(String.valueOf(A * (B.charAt(1) - '0'))+"\n");
+		bw.write(String.valueOf(A * (B.charAt(0) - '0'))+"\n");
+		bw.write(String.valueOf(A * Integer.parseInt(B)));
+		
+		bw.flush();
+		bf.close();
+		bw.close();
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int A = sc.nextInt();
+//		String B = sc.next();
+//		
+//		System.out.println(A * (B.charAt(2) - '0'));
+//		System.out.println(A * (B.charAt(1) - '0'));
+//		System.out.println(A * (B.charAt(0) - '0'));
+//		System.out.println(A * Integer.parseInt(B));
 		
 	}
 
